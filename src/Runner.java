750e9e9d2f0a96a44b405f4b711743a702cd7eee
Stanley <skbeyan@dmacc.edu>
2019-03-31 22:28:33 -0500

    initial commit

---
+++ src/Runner.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Runner {
+
+    import java.util.Arrays;
+
+    public static void main(String[] args) {
+
+            Nursing School Esther Bacon  = new nursingSchool();
+            System.out.println("nursingSchool = " + nursingSchool);
+
+    }
+    public void School() {
+
+        private List<String> employees;
+        private List<String> equipments;
+        private<List<String>> students;
+        Double cashinbank;
+        Boolean isOpen;
+}
+    }
+            }
+
+            }
